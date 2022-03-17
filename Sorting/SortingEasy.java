bfb6bdcdfc548342e55f62072281907db0de8859
J-Srinivasalu <srinivasalujuldi@gmail.com>
2022-03-17 14:43:41 +0530

    update SortingEasy

--- Sorting/SortingEasy.java
+++ Sorting/SortingEasy.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class SortingEasy {
     public static void main(String[] args) {
         int[] arr = {9, 8, 7, 5, 4, 3, 34, 23, 1, 89, 99, 1, 3, 4, 4};
+
+
         System.out.println(Arrays.toString(arr));
         quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
@@ -287,19 +289,128 @@ public class SortingEasy {
     public int largestPerimeter(int[] nums) {
         Arrays.sort(nums);
 
-        for(int i=nums.length-1;i>1;i--){
-            if(nums[i-2] + nums[i-1] > nums[i]){
-                return nums[i-2]+nums[i-1]+nums[i];
+        for (int i = nums.length - 1; i > 1; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i - 2] + nums[i - 1] + nums[i];
             }
         }
         return 0;
     }
-//    Squares of a Sorted Array
-//    Matrix Cells in Distance Order
-//    Height Checker
-//    Relative Sort Array
-//    Minimum Absolute Difference
-//    Rank Transform of an Array
+
+    //    Squares of a Sorted Array
+    public int[] sortedSquares(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        int[] ans = new int[nums.length];
+
+        for (int k = nums.length - 1; k >= 0; k--) {
+            if (Math.abs(nums[i]) > Math.abs(nums[j])) {
+                ans[k] = nums[i] * nums[i];
+                i++;
+            } else {
+                ans[k] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return ans;
+    }
+
+    //    Matrix Cells in Distance Order
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int[][] coordinates = new int[cols * rows][2];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int k = i * cols + j;
+                coordinates[k][0] = i;
+                coordinates[k][1] = j;
+            }
+        }
+        Arrays.sort(coordinates, (a, b) -> {
+            int d1 = Math.abs(rCenter - a[0]) + Math.abs(cCenter - a[1]);
+            int d2 = Math.abs(rCenter - b[0]) + Math.abs(cCenter - b[1]);
+            return d1 - d2;
+        });
+
+        return coordinates;
+    }
+
+    //    Height Checker
+    public int heightChecker(int[] heights) {
+        int[] expected = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(expected);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //    Relative Sort Array
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int largest = 0;
+        // get the largest element from arr1
+        for (int j : arr1) largest = Math.max(j, largest);
+
+        // create an array of size (largest+1) to store frequencies of elements in arr1
+        int[] frequencies = new int[largest + 1];
+        for (int j : arr1) frequencies[j]++;
+
+        int index = 0;
+        for (int j : arr2) {
+            // insert the current element of arr2 till freqeuncy of this element in arr1 becomes 0
+            while ((frequencies[j]--) > 0)
+                arr1[index++] = j;
+        }
+
+        // insert the remaining elements of arr1 as per their frequencies  which were not present in arr2
+        for (int i = 1; i < frequencies.length; i++) {
+            while ((frequencies[i]--) > 0)
+                arr1[index++] = i;
+        }
+        return arr1;
+    }
+
+    //    Minimum Absolute Difference
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> res = new ArrayList();
+        //sort elements
+        Arrays.sort(arr);
+        //init our min difference value
+        int min = Integer.MAX_VALUE;
+        //start looping over array to find real min element. Each time we found smaller difference
+        //we reset resulting list and start building it from scratch. If we found pair with the same
+        //difference as min - add it to the resulting list
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                res.clear();
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == min) {
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> minimumAbsDifference2(int[] arr) {
+        Arrays.sort(arr);
+        int minDif = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (Math.abs(arr[i + 1] - arr[i]) < minDif)
+                minDif = Math.abs(arr[i + 1] - arr[i]);
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (Math.abs(arr[i + 1] - arr[i]) == minDif)
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+        }
+        return result;
+    }
+
+    //    Rank Transform of an Array
 //    Sort Integers by The Number of 1 Bits
 //    How Many Numbers Are Smaller Than the Current Number
 //    Maximum Product of Two Elements in an Array
